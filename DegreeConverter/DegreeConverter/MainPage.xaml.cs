d7f4071f3174481b3cf9291c64198593de300965
JONATAN GOMEZ BUITRAGO <jonatangomez25261@correo.itm.edu.co>
2017-08-13 21:56:22 -0500

    Convert

---
+++ DegreeConverter/DegreeConverter/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DegreeConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+                InitializeComponent();
+                ConvertButton.Clicked += ConvertButton_Clicked;
+                OtherButton.Clicked += OtherButton_Clicked;
+            }
+            private void ConvertButton_Clicked(object sender, EventArgs e)
+            {
+                if (string.IsNullOrEmpty(DegreeEntry.Text))
+                {
+                    DisplayAlert("Error", "You must enter a value in degree.", "Accept");
+                    return;
+                }
+
+                var degree = decimal.Parse(DegreeEntry.Text);
+
+                var fahrenheit = (degree * 1.8M) + 32;
+
+
+                FahrenheitEntry.Text = string.Format("{0:N2}", fahrenheit);
+
+            }
+
+            private void OtherButton_Clicked(object sender, EventArgs e)
+            {
+                if (string.IsNullOrEmpty(DegreeEntry.Text))
+                {
+                    DisplayAlert("Error", "You must enter a value in degree.", "Accept");
+                    return;
+                }
+
+                var degree = decimal.Parse(DegreeEntry.Text);
+
+                var centigrade = (degree - 32) / 1.8M;
+
+                CentigradeEntry.Text = string.Format("{0:N2}", centigrade);
+
+            }
+        }
+    }
